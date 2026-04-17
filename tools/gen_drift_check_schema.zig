2efae33d9e291e26745409246d0e8adbf09406f9
Laurynas Keturakis <laurynas@fiberplane.com>
2026-04-17 12:04:31 +0200

    chore: fix tools/gen-check-schema for zig 0.16

--- tools/gen_drift_check_schema.zig
+++ tools/gen_drift_check_schema.zig
@@ -1,24 +1,24 @@
 const std = @import("std");
 const schema_gen = @import("drift_check_schema_gen");
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+pub fn main(init: std.process.Init) !void {
+    const gpa = init.gpa;
+    const io = init.io;
 
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
+    var iter = try init.minimal.args.iterateAllocator(gpa);
+    defer iter.deinit();
 
-    if (args.len < 2) {
-        std.debug.print("usage: {s} <output.json>\n", .{args[0]});
+    _ = iter.next(); // exe
+    const out_path = iter.next() orelse {
+        std.debug.print("usage: gen-drift-check-schema <output.json>\n", .{});
         std.process.exit(2);
-    }
+    };
 
-    var file = try std.fs.cwd().createFile(args[1], .{});
-    defer file.close();
+    var file = try std.Io.Dir.cwd().createFile(io, out_path, .{ .truncate = true });
+    defer file.close(io);
 
     var buf: [512 * 1024]u8 = undefined;
-    var w = file.writer(&buf);
+    var w = file.writer(io, &buf);
     defer w.interface.flush() catch {};
-    try schema_gen.writeJsonSchema(allocator, &w.interface);
+    try schema_gen.writeJsonSchema(gpa, &w.interface);
 }
